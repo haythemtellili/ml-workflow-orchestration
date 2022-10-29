ba5f62d317796b5adfcf2c1aab836a32ce362e2e
haythemtellili <haythem1347@gmail.com>
2022-10-29 16:51:06 +0100

    modify config file

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-user = "root"
-password = "root"
-host = "localhost:5431"
-database = "ny_taxi"
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+user = os.environ.get("user")
+password = os.environ.get("password")
+host = os.environ.get("host")
+database = os.environ.get("database")
