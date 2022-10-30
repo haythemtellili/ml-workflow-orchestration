cd57fce67468650e4c88a6392e0be2f3c480f2f7
haythemtellili <haythem1347@gmail.com>
2022-10-30 12:06:16 +0100

    add docker compose

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-user = os.environ.get("user")
-password = os.environ.get("password")
-host = os.environ.get("host")
-database = os.environ.get("database")
+user = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
+host = os.environ.get("HOST")
+database = os.environ.get("POSTGRES_DB")
